15b8169b41f67fba4acd1f6dbf0e095fd19f9751
duncanp11111 <duncanpollardschool@gmail.com>
2017-06-09 12:45:18 -0700

    Add files via upload

--- DuncanFinalAssignment.py
+++ DuncanFinalAssignment.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-#Define some colours
+#Define some colours and other variables
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -10,10 +10,9 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 movey = 3
 movex = 3
-asteroid_list = ""
-
 x = random.randrange(700)
 y = random.randrange(400)
+display_game_over = False
 
 #CLASSES
 
@@ -75,33 +74,35 @@ class Player(pygame.sprite.Sprite):
         #Set the player's x and y positions to the mouses coordinates
         self.rect.x = pos[0]
         self.rect.y = pos[1]
-        
-class SideWalls(pygame.sprite.Sprite):
-    def __init__(self):
+#Wall Class       
+class Wall(pygame.sprite.Sprite):
+    """ Class to represent the bounds of the level """
+    #Initiate class
+    def __init__(self, x, y, width, height):
         super().__init__()
-        
-        self.image = pygame.Surface([10, 10])
-        
-        pygame.draw.line(self.image, BLACK, (0, 0), (0, 400), 1)
-        pygame.draw.line(self.image, BLACK, (700, 0), (700, 400), 1)
-
-        self.rect = self.image.get_rect() 
-class TopWalls(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__() 
-        
-        self.image = pygame.Surface([10, 10])
-        
-        pygame.draw.line(self.image, BLACK, (0, 0), (700, 0), 1)
-        pygame.draw.line(self.image, BLACK, (0, 400), (700, 400), 1)   
-        
-        self.rect = self.image.get_rect()     
-        
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+#Functions to add walls to existing lists
+def add_wallx(x, y, width, height):
+    """ Places a wall and adds it to the appropriate class lists for sprite collision uses """
+    wallx = Wall(x, y, width, height)
+    wall_listx.add(wallx)
+    all_sprites_list.add(wallx)
+def add_wally(x, y, width, height):
+    """ Places a wall and adds it to the appropriate class lists for sprite collision uses """
+    wally = Wall(x, y, width, height)
+    wall_listy.add(wally)
+    all_sprites_list.add(wally)
 #Create the window
 
 #Initialize Pygame
 pygame.init()
 click_sound = pygame.mixer.Sound("coin.ogg")
+click_sound1 = pygame.mixer.Sound("SunsetLover.ogg")
+
 
 #Set perameters for the screen
 screen_width = 700
@@ -111,13 +112,40 @@ background_image = pygame.image.load("space.jpg").convert()
 
 #Define Font
 font = pygame.font.Font(None, 36)
-
+font1 = pygame.font.Font(None, 72)
 #Instructions
 pygame.mouse.set_visible(False)
 display_instructions = True
 instruction_page = 1
 name = ""
 clock = pygame.time.Clock()
+
+try: 
+    file = open('highscores.txt', 'r')
+except:
+    file = open('highscores.txt', 'w')
+    file.write("Empty\n0\nEmpty\n0\nEmpty\n0\nEmpty\n0\nEmpty\n0")
+    file.close()
+    file = open('highscores.txt', 'r')
+finally:
+    iterate = 0
+    highscore_list = []
+    name_list = []
+    for line in file:
+        iterate += 1
+        if iterate % 2 == 0:
+            highscore_list.append(line)
+        else: 
+            name_list.append(line)
+    file.close()
+
+# Clean up the lists containing the names and the scores
+for i in range(len(highscore_list)):
+    highscore_list[i].replace("\n", "")
+    highscore_list[i] = int(highscore_list[i])
+    
+for i in range(len(name_list)):
+    name_list[i] = name_list[i].replace("\n", "")
 #Instruction Loop
 done = False
 while not done and display_instructions:
@@ -135,18 +163,12 @@ while not done and display_instructions:
                     display_instructions = False
 
     screen.blit(background_image, [0 , 0])
-    
+    #Welcome screen
     if instruction_page == 1:
         """Draws title for the game"""
-        text = font.render("Space Hopper", True, RED)
-        screen.blit(text, [350, 200])
-        
-        text = font.render("Current High Score is: ", True, WHITE)
-        screen.blit(text, [100, 250])
-        
-        text = font.render("10", True, WHITE)
-        screen.blit(text, [360, 250])
-        
+        text = font1.render("Space Hopper", True, RED)
+        screen.blit(text, [200, 200])
+    #1st Instruction Page
     if instruction_page == 2:
         """Draws the instructions for page 1"""
         text = font.render("Instructions", True, WHITE)
@@ -163,7 +185,7 @@ while not done and display_instructions:
         
         text = font.render("Page 1", True, WHITE)
         screen.blit(text, [10, 120])      
-        
+    #2nd Instruction Page
     if instruction_page == 3:
         """Draws the instructions for page 2"""
         text = font.render("This program throws stars towards you.", True, WHITE)
@@ -196,21 +218,26 @@ coin_list = pygame.sprite.Group()
 
 #List of each asteroid
 asteroid_list = pygame.sprite.Group()
+wall_listx = pygame.sprite.Group()
+wall_listy = pygame.sprite.Group()
 
-#List of walls
-xwalls_list = pygame.sprite.Group()
-ywalls_list = pygame.sprite.Group()
-
-#Create Sprites
+#Create Coin
 coin = Coin(200, 200)
-
 coin.rect.x = random.randrange(700)
 coin.rect.y = random.randrange(400)
+
 #Add the coin to the coin list and the all sprites list
 coin_list.add(coin)
 all_sprites_list.add(coin)
 
-for i in range(25):
+#Add walls
+add_wallx(0, 0, 5, 400)
+add_wally(0, 0, 700, 5)
+add_wally(0, 400, 700, 5)
+add_wallx(700, 0, 5, 400)
+
+
+for i in range(20):
     #Asteroid
     asteroid = Asteroid()  
 
@@ -219,20 +246,13 @@ for i in range(25):
     all_sprites_list.add(asteroid)    
     
     #Set start position for asteroid
-    asteroid.rect.x = (screen_width - random.randrange(screen_width))
-    asteroid.rect.y = (screen_height - random.randrange(screen_height))
+    asteroid.rect.x = ((screen_width - 50) - random.randrange(500))
+    asteroid.rect.y = ((screen_height - 50) - random.randrange(300))
         
 #create player and add to all sprites list
 player = Player()
 all_sprites_list.add(player)
 trashvar = True
-
-sidewalls=SideWalls()
-all_sprites_list.add(sidewalls)
-xwalls_list.add(sidewalls)
-topwalls=TopWalls()
-all_sprites_list.add(topwalls)
-ywalls_list.add(topwalls)
 
 #Loop until closed
 done = False
@@ -251,31 +271,8 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN or pygame.QUIT:
-            
-                file = open('highscores.txt', 'w')
-                writescore = str(score) + "\n"
-                writename = name + "\n"                   
-                if score > highscores_list[0]:
-                    highscores_list.insert(0, score)
-                    highscores_list.insert(1, name)
-                elif score <= highscores_list[0] and score > highscores_list[4]:
-                    highscores_list.insert(2, score)
-                    highscores_list.insert(3, name)
-                elif score <= highscores_list[2] and score > highscores_list[6]:
-                    highscores_list.insert(4, score)
-                    highscores_list.insert(5, name)
-                elif score <= highscores_list[4] and score > highscores_list[8]:
-                    highscores_list.insert(6, score)
-                    highscores_list.insert(7, name)
-                elif score <= highscores_list[6] and score > highscores_list[10]:
-                    highscores_list.insert(4, score)
-                    highscores_list.insert(9, name)
-                elif score <= highscores_list[8] and score > 0:
-                    highscores_list.insert(10, score)  
-                    hgihscores_list.insert(11, name)
-                file.write(highscores_list)
-                file.close
+            if event.key == pygame.K_RETURN:
+                done = True
                 
                
     #Game logic
@@ -284,6 +281,9 @@ while not done:
     if asteroid.rect.y <= 0 or asteroid.rect.y >= 400:
         movey *= -1
 
+    #Speed increses after certain scores are reached
+    if score >= 0:
+        click_sound1.play()
     if score == 10 and trashvar:
         movex = 5
         movey = 5
@@ -311,9 +311,11 @@ while not done:
     #See if the player collided with an asteroid
     if pygame.sprite.spritecollide(player, asteroid_list, True):
         print ("Game Over")
-        print ("Your score is:", score)        
+        print ("Your score is:", score)      
+        display_game_over = True
         done = True
         
+    #See if player collided with coin
     if pygame.sprite.spritecollide(player, coin_list, False):
         score += 1
         print (score)
@@ -321,13 +323,29 @@ while not done:
         coin.rect.y = random.randrange(400)
         click_sound.play()
         
-    if pygame.sprite.spritecollideany(sidewalls, asteroid_list, False):
+    #See if asteroid collided with side walls
+    if pygame.sprite.groupcollide(wall_listx, asteroid_list, False, False):
         movex *= -1
         
-    if pygame.sprite.spritecollideany(topwalls, asteroid_list, False):
+    #See if asteroid collided with top walls    
+    if pygame.sprite.groupcollide(wall_listy, asteroid_list, False, False):
         movey *= -1
         
-        
+    #Highscores
+    if display_game_over:
+        for i in range(5):
+            if not score > int(highscore_list[i]):
+                continue
+            else:
+                highscore_list.insert(i, score)
+                name_list.insert(i, name)
+                break
+        with open('highscores.txt', 'w') as file:
+            for i in range(5):
+                file.write(str(name_list[i]) + "\n")
+                file.write(str(highscore_list[i]) + "\n")  
+        print ("Check highscore.txt for highscore list")
+    
     clock.tick(60)
    
     pygame.display.flip()
